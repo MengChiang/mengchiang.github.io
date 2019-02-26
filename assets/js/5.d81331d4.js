4e9e442b193dfdbedff84de8e417cc90ac98c2dc
MengChiang <meng.chiang@gmail.com>
2019-02-27 00:41:31 +0800

    add /blog

---
+++ assets/js/5.d81331d4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{178:function(t,n,e){"use strict";e.r(n);var s=e(0),l=Object(s.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"content"},[n("p",[this._v("Welcome on Morris blog")]),this._v(" "),n("BlogIndex")],1)},[],!1,null,null,null);n.default=l.exports}}]);
